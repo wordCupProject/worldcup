6196a16324191d5002c58851298304c6204f93d0
MohAitMesskine <aitmeskine.mohamed@gmail.com>
2025-07-06 02:45:42 +0100

    ajouter espace admin

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { Routes } from '@angular/router';
 import { DashboardPage } from './pages/dashboard/dashboard.page';
 import { InscriptionPage } from './pages/inscription/inscription.page';
 import { LoginPage } from './pages/login/login.page';
+import{HotelsPage} from './pages/hotels/hotels.page'
 import { adminRoutes } from './admin/admin.routes'; // Import des routes admin
 
 export const routes: Routes = [
   { path: '', component: DashboardPage },
   { path: 'inscription', component: InscriptionPage },
   { path: 'login', component: LoginPage },
+   { path: 'hotels', component: HotelsPage },
   
   // Route pour l'admin avec des enfants
   { 
